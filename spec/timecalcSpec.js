397ea41aeed790c058611a999307bae683377fce
John Lehmann <john.lehmann@gmail.com>
2013-01-19 14:08:13 -0600

    initial commit

---
+++ spec/timecalcSpec.js
@@ -0,0 +1,15 @@
+
+
+describe("Hello world", function() {
+    it("says hello", function() {
+        expect(helloWorld()).toEqual("Hello world!");
+    });
+});
+
+describe("Hello world2", function() {
+    it("says hello", function() {
+        expect(helloWorld2()).toEqual("Hello world!");
+    });
+});
+
+
---
+++ src/timecalc.js
@@ -0,0 +1,9 @@
+
+
+function helloWorld() {
+    return "Hello world!";
+}
+
+function helloWorld2() {
+    return "Hello world2!";
+}
