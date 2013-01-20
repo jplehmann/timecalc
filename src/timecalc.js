347648c3637dce1b4dbb9fd24846c9abe61b6cc1
John Lehmann <john.lehmann@gmail.com>
2013-01-19 22:38:00 -0600

    error handling

--- src/timecalc.js
+++ src/timecalc.js
@@ -4,7 +4,8 @@ $( document ).ready(function() {
   $('input:visible:first').first().focus();
   // when leaving a cell, update the totals
   $("input").blur(function(event) {
-    updateCurrentRow(event.target);
+    var $curRow = $(event.target).parents('tr');
+    updateRow($curRow);
     updateTotalColumn();
   });
   // clicking on an input will select all so as to replace
@@ -24,20 +25,55 @@ $( document ).ready(function() {
 
 /**
  * Recompute hours for a row.
+ * @param $curRow is the 'tr'
  */
-function updateCurrentRow(target) {
-  // get target's parent
-  var $trParent = $(target).parents("tr");
-  var $allInputs = $trParent.find("input");
-  // get all the inputs in an array
-  timeValues = [];
-  $allInputs.each(function() {
-      timeValues.push(this.value);
-    });
-  // perform arithmetic
-  var total = addTimes(timeValues);
-  // update total
-  $trParent.find("div.day.total").text(total);
+function updateRow($curRow) {
+  var totalTime = computeRow($curRow);
+  if (totalTime === undefined) { 
+    totalTime = ""; 
+  }
+  $total = $curRow.find("div.day.total").text(totalTime)
+  if (totalTime < 0) {
+    $total.addClass('error');
+  }
+  else {
+    $total.removeClass('error');
+  }
+}
+
+/**
+ * Using values in current row, compute hours worked.
+ * @return undefined if not valid input.
+ */
+function computeRow($curRow) {
+  // get A B and C, and parse
+  var $inputs = $curRow.find('input');
+  var timeIn = parseTimeToDuration($inputs.eq(0));
+  var timeOut = parseTimeToDuration($inputs.eq(1));
+  if (timeIn === undefined || timeOut == undefined) {
+    return undefined;
+  }
+  var breakLen = parseFloat($inputs.eq(2).val()) || 0;
+  // clear out the break field if set to 0 so it's clear
+  // that we didn't use it
+  if (breakLen === 0) {
+    $inputs.eq(2).val("");
+  }
+  //console.log("ti: " + timeIn + ", to: " + timeOut);
+  return timeOut-timeIn-breakLen;
+}
+
+/**
+ * Convert this input's value to some kind of duration
+ * else return undefined.
+ */
+function parseTimeToDuration($input) {
+  val = $input.val();
+  fval = parseFloat(val);
+  if (!fval && fval !== 0) {
+    return undefined;
+  }
+  return fval;
 }
 
 /**
@@ -59,7 +95,8 @@ function addTimes(times) {
   var total = 0;
   times.forEach(function(x) {
     // assume it's an hour total
-    if (x !== "") {
+    // ignore if blank, NaN, undefined, etc. (or 0)
+    if (x) {
       total += parseFloat(x);
     }
   });
