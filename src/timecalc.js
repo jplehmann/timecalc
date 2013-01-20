7799b1ba1f0c6572fa916bc04e754b9e6923341d
John Lehmann <john.lehmann@gmail.com>
2013-01-19 23:41:16 -0600

    smart parsing of time input

--- src/timecalc.js
+++ src/timecalc.js
@@ -1,4 +1,6 @@
 
+var PRECISION = 2;
+
 $( document ).ready(function() {
   // autofocus on the first input
   $('input:visible:first').first().focus();
@@ -31,6 +33,8 @@ function updateRow($curRow) {
   var totalTime = computeRow($curRow);
   if (totalTime === undefined) { 
     totalTime = ""; 
+  } else {
+    totalTime = totalTime.toFixed(PRECISION);
   }
   $total = $curRow.find("div.day.total").text(totalTime)
   if (totalTime < 0) {
@@ -48,9 +52,9 @@ function updateRow($curRow) {
 function computeRow($curRow) {
   // get A B and C, and parse
   var $inputs = $curRow.find('input');
-  var timeIn = parseTimeToDuration($inputs.eq(0));
-  var timeOut = parseTimeToDuration($inputs.eq(1));
-  if (timeIn === undefined || timeOut == undefined) {
+  var timeIn = parseTime($inputs.eq(0));
+  var timeOut = parseTime($inputs.eq(1), timeIn);
+  if (timeIn === undefined || timeOut === undefined) {
     return undefined;
   }
   var breakLen = parseFloat($inputs.eq(2).val()) || 0;
@@ -64,15 +68,41 @@ function computeRow($curRow) {
 }
 
 /**
- * Convert this input's value to some kind of duration
- * else return undefined.
+ * Convert this input's value to military time with decimal
+ * minute value else return undefined.
+ * e.g. 2:45pm = 14.75
+ * try to parse: 2, 2pm, 2:15, 2:15pm
+ * @param refTime reference time, assumed to be earlier, used for inference
  */
-function parseTimeToDuration($input) {
-  val = $input.val();
-  fval = parseFloat(val);
+function parseTime($input, refTime) {
+  var val = $input.val();
+  // doesn't parse 2p correctly so add a 'm' if we detect this
+  var meridiem = val && /[a|p]m/i.test(val);
+  if (val.length > 0 && 
+      (val[val.length-1] === 'p' || val[val.length-1] === 'a')) {
+    val += "m";
+    meridiem = true;
+  }
+  // let moment.js figure out what they said
+  var m = moment(val, "h:mma")
+  if (!m) {
+    return undefined;
+  }
+  // hours comes back in 0-23 range so it's already "military" time
+  var fval = m.hours() + m.minutes()/60.0;
   if (!fval && fval !== 0) {
     return undefined;
   }
+  // if this time is earlier than our reference time, make it PM if not already
+  if (!meridiem && fval < refTime) {
+    if (m.hours() < 12) {
+      fval += 12;
+      m.hours(m.hours()+12);
+    }
+  }
+  // update the field with how we understood the value
+  // TODO: pass this back to caller instead
+  $input.val(m.format("h:mma"));
   return fval;
 }
 
@@ -85,7 +115,7 @@ function updateTotalColumn() {
     return $(el).text();
   }).get();
   var total = addTimes(arr);
-  $('div.week.total').text(total);
+  $('div.week.total').text(total.toFixed(PRECISION));
 };
 
 /**
